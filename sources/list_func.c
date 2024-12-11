b75ade7b4a656b1d610b5c3f0c14c1587c39b972
Adeline <adelineeminian13@gmail.com>
2024-12-11 12:48:47 +0400

    2 maps added

--- sources/list_func.c
+++ sources/list_func.c
@@ -1,25 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lst_func.c                                      :+:      :+:    :+:   */
+/*   list_func.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adel <adel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 16:27:57 by vbarsegh          #+#    #+#             */
-/*   Updated: 2024/12/09 02:00:23 by adel             ###   ########.fr       */
+/*   Updated: 2024/12/10 18:09:15 by adel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
-t_figure	*ft_lstlast_figure(t_figure *figure)
-{
-	if (figure == NULL)
-		return (NULL);
-	while (figure && figure->next)
-		figure = figure->next;
-	return (figure);
-}
 
 t_light	*lst_create_light(t_scene *scene, char **matrix)
 {
@@ -50,6 +42,15 @@ t_light	*ft_lstlast_light(t_light *lst)
 	while (lst && lst->next)
 		lst = lst->next;
 	return (lst);
+}
+
+t_figure	*ft_lstlast_figure(t_figure *figure)
+{
+	if (figure == NULL)
+		return (NULL);
+	while (figure && figure->next)
+		figure = figure->next;
+	return (figure);
 }
 
 void	ft_lstadd_back_figure(t_figure **figure, t_figure *new)
